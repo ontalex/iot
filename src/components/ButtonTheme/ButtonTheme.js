785fc1f3bf3dd70e1341e67b8d476647fdfbbda1
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2023-07-08 01:16:42 +0300

    add: кнопки тем с переключеним

--- src/components/ButtonTheme/ButtonTheme.js
+++ src/components/ButtonTheme/ButtonTheme.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 
 import { ThemeContext } from "../context";
 
+import { ReactComponent as LightThemeBTN } from '../../assets/theme_light.svg';
+import { ReactComponent as DarkThemeBTN } from '../../assets/theme_dark.svg';
+
 import "../../styles/variables.css";
 import "./ButtonTheme.css";
 
@@ -16,7 +19,9 @@ export default function ButtonTheme() {
 
   return (
     <button className="btn" onClick={handleToggleTheme}>
-      Theme
+      {
+        theme === "light" ? <DarkThemeBTN /> : <LightThemeBTN />
+      }
     </button>
   )
 }
